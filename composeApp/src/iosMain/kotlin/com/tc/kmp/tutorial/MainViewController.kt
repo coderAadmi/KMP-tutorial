2e8750446c3e091b38dc4216138c02c5bddc9f88
LoserHasToFall98 <Pradyumn.Upadhyay@techconsulting.tech>
2026-03-10 10:36:36 -0400

    Added Todo Db builder from iosMain

--- composeApp/src/iosMain/kotlin/com/tc/kmp/tutorial/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/tc/kmp/tutorial/MainViewController.kt
@@ -1,5 +1,6 @@
 package com.tc.kmp.tutorial
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.tc.kmp.tutorial.db.getDatabaseBuilder
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController { App(getDatabaseBuilder().build()) }
