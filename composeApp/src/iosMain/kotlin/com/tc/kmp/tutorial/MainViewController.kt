ded7ffbceff2caf43d7614c28ef9198798e4e5ff
LoserHasToFall98 <Pradyumn.Upadhyay@techconsulting.tech>
2026-03-10 11:21:07 -0400

    Added BundledSQLiteDriver in ios main

--- composeApp/src/iosMain/kotlin/com/tc/kmp/tutorial/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/tc/kmp/tutorial/MainViewController.kt
@@ -1,6 +1,7 @@
 package com.tc.kmp.tutorial
 
 import androidx.compose.ui.window.ComposeUIViewController
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import com.tc.kmp.tutorial.db.getDatabaseBuilder
 
-fun MainViewController() = ComposeUIViewController { App(getDatabaseBuilder().build()) }
+fun MainViewController() = ComposeUIViewController { App( (getDatabaseBuilder().setDriver(BundledSQLiteDriver()).build())) }
